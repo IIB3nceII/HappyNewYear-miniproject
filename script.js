820e6667492b42e32fa3860d16f6ea760631cc68
Bence Papp <bencepapp1101@gmail.com>
2021-01-27 11:44:16 +0100

    Timer commit message

---
+++ script.js
@@ -0,0 +1,37 @@
+const daysEL=document.getElementById('days');
+const hoursEL=document.getElementById('hours');
+const minsEL=document.getElementById('mins');
+const secondsEL=document.getElementById('seconds');
+
+const newYears='1 jan 2022';
+
+function countdown(){
+    const newYearsDate=new Date(newYears);
+    const currentDate=new Date();
+
+    const totalSeconds=(newYearsDate-currentDate)/1000;
+    const days=Math.floor(totalSeconds/3600/24);
+    const hours=Math.floor(totalSeconds/3600)%24;
+    const mins=Math.floor(totalSeconds/60)%60;
+    const seconds=Math.floor(totalSeconds)%60;
+
+    daysEL.innerHTML=formatTime(days);
+    hoursEL.innerHTML=formatTime(hours);
+    minsEL.innerHTML=formatTime(mins);
+    secondsEL.innerHTML=formatTime(seconds);
+}
+
+function formatTime(time){
+    const t=`0${time}`
+    if(time<10){
+        return t;
+    }
+    else
+    {
+        return time;
+    }
+}
+
+countdown();
+
+setInterval(countdown,1000);
